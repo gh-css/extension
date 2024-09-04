1bc8f7149d5196e0c5365d7791a513b042e7f752
Bims <dasischbims@gmail.com>
2024-09-04 23:22:22 +0200

    removed unused permission and removed checks via webnavigation

--- src/background.ts
+++ src/background.ts
@@ -4,12 +4,6 @@
     });
 }
 
-// Listen for history state changes (SPA navigation)
-chrome.webNavigation.onHistoryStateUpdated.addListener((details: chrome.webNavigation.WebNavigationTransitionCallbackDetails) => {
-    console.log("History state updated");
-    handleCssInjection(details.tabId);
-}, { url: [{ urlMatches: 'github.com' }] });
-
 // Listen for tab updates (page load or refresh)
 chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabChangeInfo) => {
     if (changeInfo.status === 'complete') {
